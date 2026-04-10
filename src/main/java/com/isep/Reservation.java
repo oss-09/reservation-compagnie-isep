3bbbcb614a01c3eff8dd561ea8896bb2e6c590d1
oss-09 <alahyaneossama@gmail.com>
2026-04-10 15:35:47 +0200

    version finale due reservation

--- src/main/java/com/isep/Reservation.java
+++ src/main/java/com/isep/Reservation.java
@@ -21,11 +21,31 @@ public class Reservation {
     public Passager getPassager() { return passager; }
     public Vol getVol() { return vol; }
 
+    public void confirmerReservation() {
+        this.statut = "confirmee";
+        vol.getPassagers().add(passager); // on ajoute le passager au vol
+        System.out.println("Reservation " + numeroReservation + " confirmee");
+    }
+
+    public void annulerReservation() {
+        this.statut = "annulee";
+        vol.getPassagers().remove(passager); // on retire le passager du vol
+        System.out.println("Reservation " + numeroReservation + " annulee");
+    }
+
+    public void modifierReservation(Vol nouveauVol) {
+        vol.getPassagers().remove(passager);
+        this.vol = nouveauVol;
+        nouveauVol.getPassagers().add(passager);
+        System.out.println("Reservation " + numeroReservation + " modifiee");
+    }
+
     public void obtenirInfos() {
-        System.out.println("Numero : " + numeroReservation);
-        System.out.println("Date : " + dateReservation);
+        System.out.println("Numero: " + numeroReservation);
+        System.out.println("Date :" + dateReservation);
         System.out.println("Statut : " + statut);
         System.out.println("Passager : " + passager.getNom());
-        System.out.println("Vol : " + vol.getNumeroVol());
+        System.out.println("Vol:" + vol.getNumeroVol());
     }
 }
+
