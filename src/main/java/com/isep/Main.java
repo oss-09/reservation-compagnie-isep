7fe9639cc72e235f902dc265592cf5763cc22055
oss-09 <alahyaneossama@gmail.com>
2026-04-17 16:02:33 +0200

    Update Main.java

--- src/main/java/com/isep/Main.java
+++ src/main/java/com/isep/Main.java
@@ -1,5 +1,7 @@
 package com.isep;
 
+import java.util.List;
+
 public class Main {
 
     // Le lien : https://github.com/oss-09/reservation-compagnie-isep
@@ -8,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Aeroport paris = new Aeroport("CDG", "Paris", "Charles de Gaulle");
+        Aeroport paris  = new Aeroport("CDG", "Paris", "Charles de Gaulle");
         Aeroport london = new Aeroport("LHR", "Londres", "Heathrow");
         Aeroport madrid = new Aeroport("MAD", "Madrid", "Adolfo Suarez");
 
@@ -21,38 +23,26 @@ public class Main {
         Vol vol1 = new Vol("AF101", paris, london, "2026-04-20 08:00", "2026-04-20 09:30");
         Vol vol2 = new Vol("AF102", paris, madrid, "2026-04-20 12:00", "2026-04-20 14:00");
 
-        // planifier les vols
         vol1.planifierVol();
         vol2.planifierVol();
-
-        // afficher le planning
         Vol.afficherPlanning();
 
-        // affecter equipage
         vol1.affecterVol(avion1, pilote1, steward1);
         vol2.affecterVol(avion2, pilote1, steward1);
 
-        // obtenir un vol par ID
-        Vol trouve = Vol.obtenirVol("AF101");
-        if (trouve != null) {
-            trouve.obtenirInfos();
-        }
-
-        // annuler un vol par ID
-        Vol.annulerVol("AF102");
-
-        // afficher planning apres annulation
-        Vol.afficherPlanning();
-
-        // tester reservation
-        Passager passager1 = new Passager("PA001", "Osama tim", "Mar", "0698765432", "TN123456");
-        Reservation res1 = new Reservation("RES001", "2025-04-10", passager1, vol1);
+        Passager passager1 = new Passager("PA001", "Osama Tim", "Paris", "0698765432", "TN123456");
+        Reservation res1   = new Reservation("RES001", "2026-04-10", passager1, vol1);
         passager1.reserverVol(res1);
 
         vol1.listingPassagers();
 
-        passager1.annulerReservation("RES001");
+        System.out.println("\n--- Lecture / Écriture de fichiers ---");
+        GestionFichiers.initialiserFichierPassagers("passagers.csv");
+        GestionFichiers.sauvegarderPassager("passagers.csv", passager1, res1);
+        GestionFichiers.afficherPassagersDepuisFichier("passagers.csv");
 
-        vol1.listingPassagers();
+        System.out.println("\n--- Recherche de vols via Amadeus API ---");
+        System.out.println("API Amadeus désactivée - manque clés");
+
     }
 }
